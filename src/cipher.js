265a5a741183ea300b0c1d3ca2c76038b5284f8c
andrea <dev.mixicruz@gmail.com>
2023-05-16 10:12:18 -0600

    Sube todo el repo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,59 @@
-const cipher = {
-  // ...
-};
 
-export default cipher;
+
+
+function inicio() {
+  document.getElementById('encode').addEventListener('click', function () {
+    const texto = document.getElementById('string').value;
+    const desplazamiento = document.getElementById('offset').value;
+    document.getElementById('output').value = cifrar(texto, desplazamiento);
+    document.getElementById('string2').value = cifrar(texto, desplazamiento);
+  }, true);
+  document.getElementById('decode').addEventListener('click', function () {
+    const texto2 = document.getElementById('string2').value;
+    const desplazamiento2 = document.getElementById('offset2').value;
+    document.getElementById('output2').value = descifrar(texto2, desplazamiento2);
+  }, true);
+}
+
+function cifrar(texto, desplazamiento) {
+  let letra, resultado = " ";
+  desplazamiento %=26; // modulo de 26, o sea que cada 26 vuelva a 1
+  const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+  let cifrado = alfabeto.slice(+desplazamiento); 
+  cifrado = cifrado + alfabeto.slice(0, desplazamiento); // que se vaya a la derecha//
+  for (let i = 0; i < texto.length; i++) { // bucle for, las letras empiezan en 0 y el largo es 25, repetir //
+    letra = texto[i]; // toma del alfabeto las letras del string //     
+    if (letra === ' '){
+      letra = ' ';
+    } else { // obtener la posicion de la letra en el afabeto //
+      const posicionOriginal = alfabeto.indexOf(letra); 
+      // obtener la nueva posicion de letra cifrada del alfabeto nuevo//
+      letra = cifrado[posicionOriginal] || letra;
+    }
+    resultado += letra; // enviamos la letra cifrada 
+  }
+  return resultado;
+}
+
+function descifrar( texto2, desplazamiento2) {
+  let letra, resultado = " ";
+  desplazamiento2 %=26; // modulo de 26, o sea que cada 26 vuelva a 1
+  const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let descifrado = alfabeto.slice(-desplazamiento2);
+  descifrado = descifrado + alfabeto.slice(0, alfabeto.length - desplazamiento2); // que se vaya a la izquierda//
+  for (let i = 0; i < texto2.length; i++) { // bucle for, las letras empiezan en 0 y el largo es 25, repetir //
+    letra = texto2[i]; // toma del alfabeto las letras del string //
+    if (letra === ' '){
+      letra = ' ';
+    } else { // obtener la posicion de la letra en el afabeto //
+      const posicionOriginal = alfabeto.indexOf(letra); 
+      // obtener la nueva posicion de letra cifrada del alfabeto nuevo//
+      letra = descifrado[posicionOriginal] || letra;
+    }
+    resultado += letra; // enviamos la letra cifrada 
+  }
+  return resultado;
+}
+
+inicio()
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import cipher from './cipher.js';
+// import cipher from './cipher.js';
 
-console.log(cipher);
+// console.log(cipher);
+
+// _offset_
+
+const mensajeInput = document.getElementById('string');
+mensajeInput.addEventListener('input',() => {
+  mensajeInput.value = mensajeInput.value.toUpperCase();
+});
+
+const mensajeInput2= document.getElementById('string2');
+mensajeInput2.addEventListener('input',() => {
+  mensajeInput2.value = mensajeInput2.value.toUpperCase();
+});
+
+
+  
